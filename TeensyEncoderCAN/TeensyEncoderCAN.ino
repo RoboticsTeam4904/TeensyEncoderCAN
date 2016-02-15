bcf35fb939f97cd7057ea1477df76c108fed88a0
Carter Turnbaugh <carturn@nuevaschool.org>
2016-02-15 09:58:57 -0800

    Added reset.

--- TeensyEncoderCAN/TeensyEncoderCAN.ino
+++ TeensyEncoderCAN/TeensyEncoderCAN.ino
@@ -25,7 +25,9 @@ void loop(void){
         data[1] = (pos >> 8) & 0xff;
         data[2] = (pos >> 16) & 0xff;
         data[3] = (pos >> 24) & 0xff;
-        Serial.println(pos);
+        Serial.print(pos);
+        Serial.print("\t");
+        Serial.println(data[2]);
 
         data[4] = 0; // Mode
 
@@ -45,6 +47,12 @@ void loop(void){
         for(int i = 5; i < 8; i++){
           data[i] = 0;
         }
+      }
+      else if(data[0] == 0x72 && data[1] == 0x65 && data[2] == 0x73 && data[3] == 0x65 && data[4] == 0x74 && data[5] == 0x65 && data[6] == 0x6e && data[7] == 0x63){
+        encoder.write(0);
+        pos = 0;
+        rate = 0;
+        Serial.println("reset");
       }
       can.write(data);
     }
